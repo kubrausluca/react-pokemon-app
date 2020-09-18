54acd636bc144b000d9efac8fedf8a4452c0ee3b
Kubra Usluca <kubra.usluca.98@gmail.com>
2020-09-18 13:52:02 +0300

    Initialize project using Create React App

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { getAllPokemon, getPokemon } from './services/pokemon';
 import PokemonList from "./PokemonList";
 import Pagination from './Pagination'
 import axios from 'axios';
@@ -11,20 +12,44 @@ function App() {
   const [loading, setLoading] = useState(true)
 
   useEffect(() => {
-    setLoading(true)
-    let cancel
-    axios.get(currentPageUrl, {
-      cancelToken: new axios.CancelToken(c => cancel = c)
-    }).then(res => {
-      setLoading(false)
-      setNextPageUrl(res.data.next)
-      setPrevPageUrl(res.data.previous)
-      setPokemon(res.data.results.map(p => p.name))
-    })
+    async function fetchData() {
+      let res = await getAllPokemon(currentPageUrl);
+      setNextPageUrl(res.next);
+      setPrevPageUrl(res.previous);
+      let pokemon = await loadingPokemon(res.results);
+      setLoading(false);
+    }
+    fetchData();
+  }, [])
 
-    return () => cancel()
+  // useEffect(() => {
+  //   setLoading(true)
+  //   let cancel
+  //   axios.get(currentPageUrl, {
+  //     cancelToken: new axios.CancelToken(c => cancel = c)
+  //   }).then(res => {
+  //     setLoading(false)
+  //     setNextPageUrl(res.data.next)
+  //     setPrevPageUrl(res.data.previous)
+  //     // setPokemon(res.data.results.map(p => p.name))
+  //     setPokemon(res.data.results)
 
-  }, [currentPageUrl])
+  //   })
+
+  //   return () => cancel()
+
+  // }, [currentPageUrl])
+
+  const loadingPokemon = async (data) => {
+    let _pokemonData = await Promise.all(data.map(async pokemon => {
+      let pokemonRecord = await getPokemon(pokemon.url);
+      return pokemonRecord
+    }))
+
+    setPokemon(_pokemonData)
+  }
+
+  console.log(pokemon);
 
   function gotoNextPage() {
     setCurrentPageUrl(nextPageUrl)
@@ -34,16 +59,27 @@ function App() {
     setCurrentPageUrl(prevPageUrl)
   }
 
-  if (loading) return "Loading..."
+  // if (loading) return "Loading..."
 
   return (
-    <>
-      <PokemonList pokemon={pokemon} />
-      <Pagination
-        gotoNextPage={nextPageUrl ? gotoNextPage : null}
-        gotoPrevPage={prevPageUrl ? gotoPrevPage : null}
-      />
-    </>
+    <div>
+      { loading ? <h1>Loading...</h1> : (
+        <>
+          <div className="grid-container">
+            {pokemon.map((pokemon, i) => {
+              return <PokemonList key={i} pokemon={pokemon} />
+            })}
+          </div>
+        </>
+      )}
+    </div>
+    // <>
+    //   <PokemonList pokemon={pokemon} />
+    //   <Pagination
+    //     gotoNextPage={nextPageUrl ? gotoNextPage : null}
+    //     gotoPrevPage={prevPageUrl ? gotoPrevPage : null}
+    //   />
+    // </>
   );
 }
 
--- src/PokemonList.js
+++ src/PokemonList.js
@@ -1,13 +1,39 @@
 import React from 'react'
 
-export default function PokemonList({ pokemon }) {
+function PokemonList({ pokemon }) {
     // {pokemon} = props.pokemon
 
     return (
-        <div>
-            {pokemon.map(p => (
-                <div key={p}>{p}</div> 
-            ))}
+        <div className="Card">
+            <div className="Card__img">
+                <img src={pokemon.sprites.front_default} alt="" />
+            </div>
+
+            <div className="Card_Name">{pokemon.name}</div>
+
+            <div className="Card__types">
+                {pokemon.types.map(type => {
+                    return <div className="Card__type">{type.type.name}</div>
+                })}
+            </div>
+
+            <div className="Card__info">
+                <div className="Card__data Card__data--weight">
+                    <p className="title">Weight</p>
+                    <p>{pokemon.weight}</p>
+                </div>
+                <div className="Card__data Card__data--height">
+                    <p className="title">Height</p>
+                    <p>{pokemon.height}</p>
+                </div>
+                <div className="Card__data Card__data--ability">
+                    <p className="title">Ability</p>
+                    <p>{pokemon.abilities[0].ability.name}</p>
+                </div>
+            </div>
+
         </div>
     )
 }
+
+export default PokemonList;
